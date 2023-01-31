9650bb7d34baf6470a47fe58fddf1bb289af24c1
miyunecadz <jvcadz@gmail.com>
2023-02-01 00:32:45 +0800

    New laravel scaffolding

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Admin;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = Admin::create([
+            'name' => 'Admin'
+        ]);
+
+        \App\Models\User::create([
+            'username' => 'admin',
+            'password' => bcrypt('1234'),
+            'user_id' => $admin->id,
+            'account_type' => 1
+        ]);
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
