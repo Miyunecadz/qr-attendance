d0eefe539f655cd67334e03d16cb7854ad9ee343
unknown <reneegortifacion@gmail.com>
2023-02-13 18:58:42 +0800

    added code on profilecontroller

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\User;
+use App\Models\Admin;
+use App\Models\Student;
+use App\Models\Faculty;
 
 class ProfileController extends Controller
 {
@@ -15,6 +19,47 @@ class ProfileController extends Controller
 
     public function update(Request $request)
     {
+        $user = [];
+        if(auth()->user()->account_type == 1) {
+           $user = Admin::find(user()->user_id);
+            $user = Admins::where('account_type', 1)->where('user_id', user()->user_id)->first();
+            $user->update ([
+                'name' => $request->name,
+
+                
+                
+            ]);
+            
+        } elseif(auth()->user()->account_type == 2) {
+            $user = Student::find(user()->user_id);
+            $user = Students::where('account_type', 2)->where('user_id', user()->user_id)->first();
+            $user->update ([
+                'name' => $request->name,
+                'department' => Str::upper($request->department),
+                'year_level' => $request->year_level,
+                'section' => $request->section,
+                'contact_number' => $request->contact_number,
+                'email' => $request->email,
+
+               
+            ]);
+        } elseif(auth()->user()->account_type == 3) 
+        {
+           $user = Faculty::find(auth()->user()->user_id);
+            $user = Faculties::where('account_type', 3)->where('user_id', user()->user_id)->first();
+            $user->update ([
+                'name' => $request->name,
+                'department' => Str::upper($request->department),
+                'position' => $request->position,
+                'contact_number' => $request->contact_number,
+                'email' => $request->email,
+
+                
+            ]);
+        }
         
+        $user->update($request->all());
+        return back()->with('success', 'Successfully updated profile information');
+       
     }
 }
