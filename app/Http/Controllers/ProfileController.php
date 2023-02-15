80f04f668dfe8b5db4f365ff01f080108febe71c
unknown <reneegortifacion@gmail.com>
2023-02-15 13:13:28 +0800

    edit profile added

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,6 +9,7 @@ use App\Models\User;
 use App\Models\Admin;
 use App\Models\Student;
 use App\Models\Faculty;
+use Illuminate\Support\Facades\Validator;
 
 class ProfileController extends Controller
 {
@@ -20,46 +21,87 @@ class ProfileController extends Controller
     public function update(Request $request)
     {
         $user = [];
-        if(auth()->user()->account_type == 1) {
-           $user = Admin::find(user()->user_id);
-            $user = Admins::where('account_type', 1)->where('user_id', user()->user_id)->first();
-            $user->update ([
-                'name' => $request->name,
-
-                
-                
-            ]);
+       
+        if(auth()->user()->account_type == 1) { 
+            $user = Admin::find(auth()->user()->user_id);
+            $validator = Validator($request->all(),[
             
-        } elseif(auth()->user()->account_type == 2) {
-            $user = Student::find(user()->user_id);
-            $user = Students::where('account_type', 2)->where('user_id', user()->user_id)->first();
+                'name' => 'required',
+               
+               
+    
+            ]);
+            if ($validator->fails()) {
+                return back()
+                            ->withErrors($validator)
+                            ->withInput();
+            }
+            $user->update ([
+                
+                
+                'name' => $request->name,
+               
+            ]);
+            return back()->with('success', 'Admin information successfully updated');
+        }
+        
+        elseif(auth()->user()->account_type == 2) { 
+            $user = Student::find(auth()->user()->user_id);
+            $validator = Validator($request->all(),[
+            
+                'name' => 'required',
+                'department' => 'required',
+                'year_level' => 'required|numeric',
+                'section' => 'required',
+                'contact_number' => 'required|numeric',
+                'email' => 'required|email',
+               
+               
+    
+            ]);
+            if ($validator->fails()) {
+                return back()
+                            ->withErrors($validator)
+                            ->withInput();
+            }
             $user->update ([
                 'name' => $request->name,
-                'department' => Str::upper($request->department),
+                'department' => $request->department,
                 'year_level' => $request->year_level,
                 'section' => $request->section,
                 'contact_number' => $request->contact_number,
                 'email' => $request->email,
-
                
             ]);
-        } elseif(auth()->user()->account_type == 3) 
-        {
-           $user = Faculty::find(auth()->user()->user_id);
-            $user = Faculties::where('account_type', 3)->where('user_id', user()->user_id)->first();
+            return back()->with('success', 'Student information successfully updated');
+        }
+        elseif(auth()->user()->account_type == 3) { 
+            $user = Faculty::find(auth()->user()->user_id);
+            $validator = Validator($request->all(),[
+            
+                'name' => 'required',
+                'department' => 'required',
+                'position' => 'required',
+                'contact_number' => 'required|numeric',
+                'email' => 'required|email',
+               
+               
+    
+            ]);
+            if ($validator->fails()) {
+                return back()
+                            ->withErrors($validator)
+                            ->withInput();
+            }
             $user->update ([
                 'name' => $request->name,
-                'department' => Str::upper($request->department),
+                'department' => $request->department,
                 'position' => $request->position,
                 'contact_number' => $request->contact_number,
                 'email' => $request->email,
-
-                
+               
             ]);
+            return back()->with('success', 'Faculty information successfully updated');
         }
-        
-        $user->update($request->all());
-        return back()->with('success', 'Successfully updated profile information');
-       
     }
 }
