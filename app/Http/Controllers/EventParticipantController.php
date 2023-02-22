68be04978aa408718ba2fd1bce3d0d34fee5c817
Erzan12 <yoboijanjan30@gmail.com>
2023-02-23 03:37:14 +0800

    Added display all participant functionality and able to selected participant

--- app/Http/Controllers/EventParticipantController.php
+++ app/Http/Controllers/EventParticipantController.php
@@ -8,14 +8,24 @@ use Illuminate\Http\Request;
 
 class EventParticipantController extends Controller
 {
-    public function index()
+    public function index($event_id)
     {
-        $participants = [];
+        $participants = EventParticipant::where('event_id', $event_id)->get();
+
         return view('events.participants.index', compact('participants'));
     }
 
-    public function destroy(Request $request)
+public function destroy(Request $request)
     {
-        dd($request->all());
+        $participantIds = $request->input('participants');
+
+        if (count($participantIds) < 1) {
+            return redirect()->back()->with('error', 'At least one participant ID is required.');
+        }
+
+        EventParticipant::whereIn('id', $participantIds)->delete();
+
+            return redirect()->back()->with('success', 'Participants have been successfully deleted.');
     }
+
 }
