08dd13520b4af324ca8e91c89fee8b50486d236e
miyunecadz <jvcadz@gmail.com>
2023-03-04 23:29:59 +0800

    Add attendance report

---
+++ app/Http/Controllers/AttendanceReportController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Event;
+use Illuminate\Http\Request;
+use App\Models\EventParticipant;
+use Illuminate\Support\Facades\DB;
+
+class AttendanceReportController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        $events = Event::select(['id', 'title'])->get();
+        $participants = [];
+        $participantAbsentCount = $participantPresentCount = 0;
+        if($request->has('event')) {
+            $studentParticipants = EventParticipant::leftJoin('students', 'event_participants.user_id', '=', 'students.id')
+                ->select([
+                    'id_number',
+                    'name',
+                    'department',
+                    DB::raw('(CASE WHEN students.id > 0 THEN 2 END) as user_type'),
+                    'is_present',
+                ])
+                ->where('event_id', $request->event)
+                ->where('user_type', 2);
+
+            $facultyParticipants = EventParticipant::leftJoin('faculties', 'event_participants.user_id', '=', 'faculties.id')
+                ->select([
+                    'employee_id as id_number',
+                    'name',
+                    'department',
+                    DB::raw('(CASE WHEN faculties.id > 0 THEN 3 END) as user_type'),
+                    'is_present',
+                ])
+                ->where('event_id', $request->event)
+                ->where('user_type', 3);
+            
+            $participants = $facultyParticipants->union($studentParticipants)
+                ->get()
+                // ->where('is_present', true)
+                ->groupBy('department');
+
+            $participantPresentCount = $participants->map(function($value, $key){
+                return $value->filter(function($value, $key) {
+                    return $value->is_present == true;
+                })->count();
+            });
+
+            $participantAbsentCount = $participants->map(function($value, $key){
+                return $value->filter(function($value, $key) {
+                    return $value->is_present == false;
+                })->count();
+            });
+
+            // foreach($participants as $key => $value) {
+            //     dd($participantPresentCount[$key]. '-'.$participantAbsentCount[$key]);
+            // }
+        }
+
+        return view('reports.attendance', compact('events', 'participants', 'participantAbsentCount', 'participantPresentCount'));
+    }
+}
---
+++ resources/views/reports/attendance.blade.php
@@ -0,0 +1,102 @@
+@extends('layouts.app')
+
+@section('content')
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">{{ __('Attendance Report') }}</h1>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <form action="" method="get" class="row">
+                                <select name="event" id="event" class="events col-md-4 col-lg-4 form-control my-2 my-md-0 mr-md-2">
+                                    <option value="">Select event</option>
+                                    @foreach ($events as $event)
+                                        <option value="{{$event->id}}" @selected(request()->event == $event->id)>{{$event->title}}</option>
+                                    @endforeach
+                                </select>
+
+                                <button type="submit" class="col-md-2 col-lg-1 btn btn-sm btn-primary my-2 my-md-0 ml-md-2">Generate</button>
+                            </form>
+                        </div>
+                    </div>
+
+                    @if(count($participants) > 0)
+                        <div class="card">
+                            <div class="card-body">
+                                <nav>
+                                    <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                                        @foreach ($participants as $key => $value)
+                                        <button class="nav-link" id="nav-{{$key}}-tab" data-toggle="tab" data-target="#nav-{{$key}}" type="button" role="tab" aria-controls="nav-{{$key}}" aria-selected="false">
+                                            {{$key}}
+                                            <div class="d-flex">
+                                                <span class="mx-1 badge badge-success" title="Number of Present Participants">{{ $participantPresentCount[$key] }}</span>
+                                                <span class="ml-1 badge badge-danger" title="Number of Absent Participants">{{ $participantAbsentCount[$key] }}</span>
+                                            </div>
+                                        </button>
+                                        @endforeach
+                                    </div>
+                                </nav>
+
+                                <div class="tab-content mt-1" id="nav-tabContent">
+                                    @foreach ($participants as $key => $value)
+                                        <div class="tab-pane fade show" id="nav-{{$key}}" role="tabpanel" aria-labelledby="nav-{{$key}}-tab">
+                                            <table class="table table-responsive-sm table-striped-columns table-bordered table-hover">
+                                                <thead>
+                                                    <tr>
+                                                        <th>ID Number</th>
+                                                        <th>Participant Name</th>
+                                                        <th>Participant Type</th>
+                                                        <th>Participant Status</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    @foreach ($value as $participant)
+                                                    <tr>
+                                                        <td>{{$participant->id_number}}</td>
+                                                        <td>{{$participant->name}}</td>
+                                                        <td>{{$participant->getPrettyUserType() }}</td>
+                                                        <td>{{$participant->getPrettyStatus() }}</td>
+                                                    </tr>    
+                                                    @endforeach
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    @endforeach
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+@endsection
+
+@section('styles')
+    <style scoped>
+        .select2-selection__rendered {
+            line-height: 18px !important;
+        }    
+    </style>    
+@endsection
+
+@section('scripts')
+    <script defer>
+        $('.events').select2();
+    </script>
+@endsection
